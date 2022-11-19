31983391f0e850471d986be1bcac2b9c52767be0
Merzough Münker <mmuenker@digitaix.com>
2022-11-19 12:56:24 +0100

    fix: use root permissions to delete temp data folder

--- restore-from-backup.sh
+++ restore-from-backup.sh
@@ -8,6 +8,6 @@ mkdir -p /tmp/data
 
 tar -xf $1 -C /tmp/data
 
-docker run --rm --user "$(id -u):$(id -g)" -v vaultwarden_data:/data -v "/tmp/data:/restore" alpine mv /restore /data
+docker run --rm -v vaultwarden_data:/data -v "/tmp/data:/restore" alpine mv /restore /data
 
-rm -fr /tmp/data
+sudo rm -fr /tmp/data
