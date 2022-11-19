962a7d3870971bdc8ba3a1d59021b0cad0035203
Merzough Münker <mmuenker@digitaix.com>
2022-11-19 12:55:31 +0100

    fix: run restore with user permissions

--- restore-from-backup.sh
+++ restore-from-backup.sh
@@ -4,7 +4,10 @@ docker compose down
 
 docker volume create vaultwarden_data || true
 
+mkdir -p /tmp/data
+
 tar -xf $1 -C /tmp/data
 
-docker run -v vaultwarden_data:/data -v "/tmp/data:/restore" alpine mv /restore /data
+docker run --rm --user "$(id -u):$(id -g)" -v vaultwarden_data:/data -v "/tmp/data:/restore" alpine mv /restore /data
 
+rm -fr /tmp/data
