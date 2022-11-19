9431310b13ef60f806c208d5a10a6f5e4d53328d
Merzough Münker <mmuenker@digitaix.com>
2022-11-19 13:39:55 +0100

    fix: typo

--- backup/run-rclone.sh
+++ backup/run-rclone.sh
@@ -3,5 +3,5 @@
 echo "sync with rclone"
 
 REMOTE=$(rclone --config /rclone.conf listremotes | head -n 1)
-echo "use rclone remote >$RMOTE<"
+echo "use rclone remote >$REMOTE<"
 rclone --config /rclone.conf sync /backups $REMOTE/vaultwarden
