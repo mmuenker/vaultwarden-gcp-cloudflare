110e32954a147e627051e1c95bf204c3e99b53c5
Merzough Münker <mmuenker@digitaix.com>
2022-11-19 12:59:45 +0100

    fix: add run output

--- restore-from-backup.sh
+++ restore-from-backup.sh
@@ -8,6 +8,6 @@ mkdir -p /tmp/data
 
 tar -xf $1 -C /tmp/data
 
-docker run --rm -v vaultwarden_data:/data -v "/tmp/data:/restore" eeacms/rsync rsync -ah /restore/ /data/
+docker run --rm -v vaultwarden_data:/data -v "/tmp/data:/restore" eeacms/rsync rsync -ah --progress --stats /restore/ /data/
 
 sudo rm -fr /tmp/data
